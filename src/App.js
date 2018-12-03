e43011fb6a961dd0cc66689753971ae9b804fef8
ErickHartawan <hartawanerick@gmail.com>
2018-12-04 02:12:32 +1100

    Data fetched with Component did mount and injected

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import Lyrics from './components/tracks/Lyrics';
-
-
-import {  Provider  } from './context';
+//import {  Provider  } from './context';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { createStore } from 'redux';
+import axios from 'axios';
+import { Provider, connect } from 'react-redux';
+// reducers taken for this is used from app-7
+const reducer = (state,action) =>{
+  if (action.type == "INJECT_STATE"){
+    return{
+      state: action.state
+    };
+  }
 
+}
+const store = createStore(reducer,state);
 
 class App extends Component {
+    state = {
+      track_list:[],
+    };
+  
+  componentDidMount(){
+  axios.get(`https://infinite-lowlands-58555.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=au&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+        .then( res => {
+            // console.log(res.data)
+            // this.setState({track_list: res.data.message.body.track_list });
+            store.dispatch({
+              type:"INJECT_STATE",
+              state: res.data.message.body.track_list,
+            });
+            })
+        .catch( err =>  console.log(err))
+  }
   render() {
     return (
-      <Provider>
+      <Provider store ={store}>
       <Router>
         <React.Fragment>
           <Navbar />
