0b01cd5230db3d84c3457c703d01fc0499d7fc97
ErickHartawan <hartawanerick@gmail.com>
2018-12-12 13:41:52 +1100

    fixed app.js unupdated

--- src/App.js
+++ src/App.js
@@ -3,52 +3,14 @@ import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import Lyrics from './components/tracks/Lyrics';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-<<<<<<< HEAD
-import axios from 'axios';
-import { Provider, connect } from 'react-redux';
-import { store } from './index'
 // reducers taken for this is used from app-7
 
-export function reducer(state,action){
-  if (action.type === "INJECT_STATE"){
-    return {...state,track_list: action.payload}
-  } else {
-    return state;
-  }
-}
-=======
-// reducers taken for this is used from app-7
-
->>>>>>> redux-rewrite
 
 
 class App extends Component {
-    state = {
-      track_list:[],
-    };
-  
-  componentWillMount(){
-  axios.get(`https://infinite-lowlands-58555.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=au&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
-        .then( res => {
-            // console.log(res.data);
-            // this.setState({track_list: res.data.message.body.track_list });
-            const result = res.data.message.body.track_list;
-            this.setState({track_list : result})
-            // console.log(this.state)
-            store.dispatch({
-              type:"INJECT_STATE",
-              payload: this.state.track_list,
-              });
-            console.log(store.getState());
-            })
-        .catch( err =>  console.log(err))
-  }
+
   render() {
     return (
-<<<<<<< HEAD
-      <Provider store ={store}>
-=======
->>>>>>> redux-rewrite
       <Router>
         <React.Fragment>
           <Navbar />
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -7,51 +7,12 @@ import { store } from '../../index';
 import { connect } from 'react-redux';
 
 class Tracks extends Component {
-<<<<<<< HEAD
-  render() {
-    return (
-        <Consumer>
-        { 
-        value => {
-          const { track_list, heading } = value ;
-
-          if (track_list === undefined || track_list.length === 0){
-            return (
-              <Spinner />
-            );
-          } else {
-            return (
-              
-              <React.Fragment>
-                <Search />
-              <h1 className='text-center mb-4'>
-              {heading}
-              </h1>
-              <div className='row'>
-              {track_list.map(items => (
-                <Track key={items.track.track_id} track={items.track} />
-                  
-                  )
-                )
-              }
-              </div>
-              </React.Fragment>
-            );
-          }
-        }
-      }
-        </Consumer>
-    );
-  }
-}  
-=======
   state={};
 
   // componentDidMount() {
   //   const statefromstore = store.getState();
   //   this.setState({statefromstore});
   // };
->>>>>>> redux-rewrite
 
   // componentDidUpdate(prevState) {
   //   if(this.state !== prevState.state){
