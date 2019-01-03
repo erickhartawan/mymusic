d081ebaee9a28e9ae887a3cce840d7fb2e472f21
ErickHartawan <hartawanerick@gmail.com>
2019-01-04 01:21:55 +1100

    left somefile behind from prev commit

---
+++ src/store/reducers/trackListReducer.js
@@ -0,0 +1,28 @@
+import { FETCH_DATA, FETCH_RESULT, ADD_FAVOURITE } from "../actions/types";
+
+const initialState =
+{
+  "heading":"Top 10 musics from MusixMatch",
+  "track_list": [
+    {
+      "track": {
+        "track_id": "",
+        "album_name": "",
+        "artist_nassme": "",
+        "track_name": "",
+      }
+    }
+  ]
+}
+export default function trackListReducer(state=initialState, action) {
+  if (action.type === FETCH_DATA) {
+    return {
+      ...state,
+      track_list : action.data.track_list
+  }
+    
+  }
+  else {
+    return state;
+  }
+}
---
+++ src/store/reducers/trackResultReducer.js
@@ -0,0 +1,26 @@
+import { FETCH_DATA, FETCH_RESULT, ADD_FAVOURITE } from "../actions/types";
+
+const initialState =
+{
+  "track_result": [
+    {
+      "track": {
+        "track_id": "",
+        "album_name": "",
+        "artist_name": "",
+        "track_name": "",
+      }
+    }
+  ],
+}
+
+export default function trackResultReducer(state=initialState, action) {
+  if (action.type === FETCH_RESULT) {
+    return{
+      ...state,
+      track_result: action.data.track_list,
+    };
+  } else {
+    return state;
+  }
+}
